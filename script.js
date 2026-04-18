e345c14767d92b1ff3c84f6cea019a3d0a1344fa
Priyanshi G <priyanshigarg111@icloud.com>
2026-04-18 19:17:54 -0400

    adding to the units

--- script.js
+++ script.js
@@ -22,11 +22,62 @@ const Units = [
         leq: {
             prompts: [
                 "Some historians consider the Mongols' military success and vast commercial empire in the 13th century as the beginning of the early modern era. Develop an argument that evaluates the extent to which the Mongol Empire was significant in larger patterns of continuity or change between 1200-1450.",
-                ""
+                "Goods and ideas flowed through the African and Eurasian trade networks in the period from 1200-1450. Develop an argument that evaluates the extent to which the various networks of exchange in the time and place were similar or different.",
+                "Weather patterns, foods, and diseases affected exchange networks in Eurasia and Africa in the period from 1200 to 1450. Develop an argument that evaluates the extent to which various networks of exchange in Afro-Eurasia during the time affected the environment.",
+                "ALthough the primary purpose of most African and Eurasian trading networks between 1200 and 1450 was economic, ideas and art also traveled through them. Develop an argument that evaluates the extent to which trading networks in these regions between 1200 and 1450 affected the diffusion of cultural traditions."
             ]
+        },
+        dbq: {
+            prompt: "Using the documents and your knowledge of world history, evaluate the extent to which women exercised power and influence within various societies in Afro-Eurasia during the period c. 1200-c. 1450.",
+            note: "assests/notes/unit2DBQ.pdf"
         }
     },
     {id: 3, title: "Land-Based Empires", period: "c. 1450 to c. 1750", 
         desc: "Focuses on the expansion, administration, and legitimization of land-based gunpowder empires.", 
-        subs: ["3.1", "3.2", "3.3", "3.4"]}
+        subs: ["3.1", "3.2", "3.3", "3.4"],
+        leq: {
+            prompts: [
+                "In Africa and Eurasia from 1450 to 1750, various forms of Islam and Christianity were key factors in the expansion of empires. Develop an argument that evaluates the extent to which the various belief systems showed continuity or change over time during the period from 1450 to 1750.",
+                "Rulers of empires in Africa and Eurasia from 1450 to 1750 developed new methods of governing to solidify their authority over their citizens. Develop an argument that evaluates the extent to which changes in government caused the consolidation of power in empires in the period 1450 to 1750.",
+                "New technology, including gunpowder weapons, shaping empires in throughout southern and southwestern Asia from 1450 to 1750. Develop an argument that evaluates the extent to which various land-based empires in southern and southwestern Asia developed in similar ways from 1450 to 1750.",
+                "New technology, including gunpowder weapons, shaped empires in Russia and China from 1450 to 1750. Develop an argument that evaluates the extent to which various land-based empires in Russia and China developed in similar ways from 1450 to 1750."
+            ]
+        },
+        dbq: {
+            prompt: "Using the documents and your knowledge of world history, evaluate the extent to which Peter the Great took steps during his reign (1682-1725) to modernize Russia and transform it into a major European power.",
+            note: "assests/notes/unit3DBQ.pdf"
+        }
+    },
+    {id: 4, title: "Transoceanic Interconnections", period: "c. 1450 to c. 1750", 
+        desc: "Focuses on the rise of European maritime empires, the linking of Eastern and Western hemispheres, and the resulting global exchange of goods, diseases, and people.", 
+        subs: ["4.1", "4.2", "4.3", "4.4", "4.5", "4.6", "4.7", "4.8"],
+        leq: {
+            prompts: [
+                "In the period from 1450 to 1750, trans-Atlantic trade brought new inventions and ideas to societies in the Eastern and Western hemispheres. Develop an argument that evaluates the extent to which cross-cultural interactions resulted in the diffusion of technology and helped cause changes in patterns of trade and travel during that era.",
+                "Strong central governments, mercantilism, and slave trade all shaped Europeans' transoceanic explorations from 1450 to 1750. Develop an argument that evaluates the extent to which economic systems and labor systems showed continuities or changes over time during that period.",
+                "The Columbian Exchange brought riches to Europe from 1450 to 1750, but it also brought misery to the Americas and Africa. Develop an argument that evaluates the extent to which the systems of slavery in that era showed changes or continuities over time.",
+                "A desire to convert others, the development of syncretic belief systems, and religious conflicts changed the Americas, Africa, and Eurasia from 1450 to 1750. Develop an argument that evaluates the extent to which the effect on societies of various belief systems was similar or different that time period."
+            ]
+        },
+        dbq: {
+            prompt: "Using the documents and your knowledge of world history, develop an argument that evaluates the extent to which states managed to consolidate and expand their power during the period 1450 and 1750.",
+            note: "assests/notes/unit4DBQ.pdf"
+        }
+    },
+    {id: 5, title: "Revolutions", period: "c. 1750 to c. 1900", 
+        desc: "Explores the political, intellectual, and industrial revolutions that reshaped the world from 1750 to 1900.", 
+        subs: ["5.1", "5.2", "5.3", "5.4", "5.5", "5.6", "5.7", "5.8", "5.9", "5.10"],
+        leq: {
+            prompts: [
+                "In the 1800s and early 1900s, industrialization societies in Africa and Asia, but the process of industrialization varied from country to country. Develop an argument that evaluates the extent to which the process of industrialization in Egypt under Muhammad Ali and in Japan during the Meiji Era were similar or different.",
+                "Enlightenment ideals and the concepts of nationalism swept the Atlantic world from 1750 to 1900 as people developed new standards of freedom and self-determination. Developed an argument that evaluates the extent to which intellectual and ideological causes influenced the revolutions that occurred in the Atlantic world during that era.",
+                "New inventions contributed greatly to industrialization from 1750 to 1900 in Eurasia, Africa, and the Americas, but agricultural productivity and natural resources also played a part. Developed an argument that evaluates the extent to which environmental factors contributed to industrialization from 1750 to 1900.",
+                "In the period from 1750 to 1900, businesses in Eurasia, the Americas, and Africa."
+            ]
+        },
+        dbq: {
+            prompt: "Using the documents and your knowledge of world history, develop an argument that evaluates the extent to which states managed to consolidate and expand their power during the period 1450 and 1750.",
+            note: "assests/notes/unit4DBQ.pdf"
+        }
+    }
 ]
