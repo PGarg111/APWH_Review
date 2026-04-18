b28aa2673adf1ec55c7c3cb1dc8a69d7607daa56
Priyanshi G <priyanshigarg111@icloud.com>
2026-04-18 16:31:07 -0400

    adding the javascript for the website, adding the units information for the units tab

---
+++ script.js
@@ -0,0 +1,32 @@
+const Units = [
+    {id: 1, title: "The Global Tapestry", period: "c. 1200 to c. 1450", 
+        desc: "Focuses on state-building technological advancements, and cultural developments around the world during the time period.", 
+        subs: ["1.1", "1.2", "1.3", "1.4", "1.5", "1.6", "1.7"], 
+        leq: {
+        prompt: "By the 14th century, Islam, Hinduism, and Confucianism each included movements within them that placed greater emphasis on either emotion or reason. Develop and argument that evaluates the extent to which two religious or ethical systems of thought were similar or different up through the 15th century.",
+        prompts: [
+            "By the 14th century, Islam, Hinduism, and Confucianism each included movements within them that placed greater emphasis on either emotion or reason. Develop and argument that evaluates the extent to which two religious or ethical systems of thought were similar or different up through the 15th century.",
+            "Because of its size, wealth, and cultural innovations, China had a significant influence on ite East Asia neighbors in the years 1200 to 1450-wether those neighbors were friendly or hostile. Develop an argument that evaluates the extent to which Chinese cultural traditions led to political or social change in other East Asian societies in that time period.",
+            "Abundant natural resources and the rise of powerful centralized governments affected Africans states between the 12th and the 15th centuries. Develop an argument that evaluates the extent to which change or continuity over time occurred in the economics and politics during that time period in one African state.",
+            "Abundant natural resources and the rise of powerful centralized governments affected American states between the 12th and the 15th centuries. Develop an argument that evaluates the extent to which change or continuity over time occurred in the economics and politics during that time period in one American state."
+        ]
+        },
+        dbq: {
+            prompt: "Using the documents and your knowledge of world history, evaluate the extent to which contact among groups of people influenced cultural developments in sub-Saharan African during the time period c. 1200 to c. 1450. The documents have been edited for the purpose of this exercise. You are advised to spend 15 minutes planning and 45 minutes writing your answer.",
+            note:"assests/notes/unit1DBQ.pdf"
+        }
+    },
+    {id: 2, title: "Networks of Exchange", period: "c. 1200 to c. 1450", 
+        desc: "Focuses on the expansion of trade routes which connected Afro-Eurasia.", 
+        subs: ["2.1", "2.2", "2.3", "2.4", "2.5", "2.6", "2.7"],
+        leq: {
+            prompts: [
+                "Some historians consider the Mongols' military success and vast commercial empire in the 13th century as the beginning of the early modern era. Develop an argument that evaluates the extent to which the Mongol Empire was significant in larger patterns of continuity or change between 1200-1450.",
+                ""
+            ]
+        }
+    },
+    {id: 3, title: "Land-Based Empires", period: "c. 1450 to c. 1750", 
+        desc: "Focuses on the expansion, administration, and legitimization of land-based gunpowder empires.", 
+        subs: ["3.1", "3.2", "3.3", "3.4"]}
+]
